c00a37916b3bda7f40c28112e4bf57e031de95e8
Aayash <dangaayash@gmail.com>
2026-07-15 12:41:01 +0545

    use web rtc update

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -87,7 +87,21 @@ export const useWebRTC = ({ isInitiator, targetId, onCallEnded }) => {
           iceServers: [
             { urls: "stun:stun.l.google.com:19302" },
             { urls: "stun:stun1.l.google.com:19302" },
-            { urls: "stun:stun2.l.google.com:19302" },
+            {
+              urls: "turn:openrelay.metered.ca:80",
+              username: "openrelayproject",
+              credential: "openrelayproject",
+            },
+            {
+              urls: "turn:openrelay.metered.ca:443",
+              username: "openrelayproject",
+              credential: "openrelayproject",
+            },
+            {
+              urls: "turn:openrelay.metered.ca:443?transport=tcp",
+              username: "openrelayproject",
+              credential: "openrelayproject",
+            },
           ],
         });
         pcRef.current = pc;
